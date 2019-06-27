44c53c984f7f11bad79dbc9a574f2f31c4f2f7cb
steven <291766389@qq.com>
2019-06-27 15:50:51 +0800

    增加mybatis

---
+++ src/main/java/com/demo/model/UserDO.java
@@ -0,0 +1,93 @@
+package com.demo.model;
+
+public class UserDO {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String sex;
+
+    private String groups;
+
+    private String department;
+
+    private String authority;
+
+    private String job;
+
+    private String teleNumber;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getGroups() {
+        return groups;
+    }
+
+    public void setGroups(String groups) {
+        this.groups = groups == null ? null : groups.trim();
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department == null ? null : department.trim();
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority == null ? null : authority.trim();
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job == null ? null : job.trim();
+    }
+
+    public String getTeleNumber() {
+        return teleNumber;
+    }
+
+    public void setTeleNumber(String teleNumber) {
+        this.teleNumber = teleNumber == null ? null : teleNumber.trim();
+    }
+}
