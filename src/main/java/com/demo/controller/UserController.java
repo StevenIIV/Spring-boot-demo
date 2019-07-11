e95af9a4c74a46b99b188bf21d833cd1f5a6808a
steven <291766389@qq.com>
2019-07-11 17:42:35 +0800

    新增 swagger2 功能

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.demo.controller;
 
 import com.demo.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 
+@Api
 @RequestMapping("/user")
 @Controller
 public class UserController {
     @Resource
     private UserService userService;
 
+    @ApiOperation(value = "get username", notes = "11")
     @GetMapping("/userlist")
     public String name(Model model){
         String name = userService.getUserDOById(1).getUsername();
